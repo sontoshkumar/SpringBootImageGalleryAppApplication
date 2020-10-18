fdb415b33bfa61c0e540a5fff9bacf8c58ba6a58
gcc <gcc@DESKTOP-4F1E50B>
2020-10-18 13:00:40 +0530

    ff

--- src/main/java/com/pixeltrice/springbootimagegalleryapp/SpringBootImageGalleryAppApplication.java
+++ src/main/java/com/pixeltrice/springbootimagegalleryapp/SpringBootImageGalleryAppApplication.java
@@ -8,7 +8,7 @@ public class SpringBootImageGalleryAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootImageGalleryAppApplication.class, args);
-		System.out.println("hi");
+		
 	}
 
 }
